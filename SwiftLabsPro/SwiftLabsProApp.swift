b31338709dcfe44a4fb1cf82caa3294bed7e92d2
Dydy003 <dcaetano8@icloud.com>
2024-11-25 21:58:00 +0100

    Initial Commit

---
+++ SwiftLabsPro/SwiftLabsProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLabsProApp.swift
+//  SwiftLabsPro
+//
+//  Created by Dylan Caetano on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLabsProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
